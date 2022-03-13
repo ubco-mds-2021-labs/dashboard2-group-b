4e2fba17a33c897f77f740fb8cf52f3f4f81945e
Harpreet Kaur <87296080+harpreetkaurguglani@users.noreply.github.com>
2022-03-13 12:37:15 -0700

    Update app.R

--- src/app.R
+++ src/app.R
@@ -8,12 +8,16 @@ library(plotly)
 library(patchwork)
 
 energy_data<-read.csv('../data/energydata_complete.csv')
+drops <- c("rv1","rv2")
+energy_data<-energy_data[ , !(names(energy_data) %in% drops)]
 
 sum(is.na(energy_data))
 #No NA values present in the data
 
 #Change column names
-colnames(appdata) <- c("Date", "Appliances","Lights","Kitchen_temp","Kitchen_hum","Living_temp","Living_hum","Laundry_temp","Laundry_hum","Office_temp","Office_hum","Bathroom_temp","Bathroom_hum","NS_temp","NS_hum","Ironroom_temp","Ironroom_hum","Teenroom_temp","Teenroom_hum","Parentsroom_temp","Parentsroom_hum","Temp_out","Pressure","Humidity_out","Windspeed","Visibility","Dewpoint")
+colnames(energy_data) <- c("Date", "Appliances","Lights","Kitchen_temp","Kitchen_hum","Living_temp","Living_hum","Laundry_temp","Laundry_hum","Office_temp","Office_hum","Bathroom_temp","Bathroom_hum","NS_temp","NS_hum","Ironroom_temp","Ironroom_hum","Teenroom_temp","Teenroom_hum","Parentsroom_temp","Parentsroom_hum","Temp_out","Pressure","Humidity_out","Windspeed","Visibility","Dewpoint")
 
 #Change to date type
-appdata$Date<-ymd_hms(appdata$Date)
+energy_data$Date <- ymd_hms(energy_data$Date)
+
+energy_data$Month <- months(energy_data$Date)
